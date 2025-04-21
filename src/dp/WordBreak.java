5e6b6f9dc74f3f75045269d58e2ce9b6541ddec4
Mustafa-Ali-iitb <mustafaali5632016@gmail.com>
2025-04-21 16:52:39 -0400

    WordBreak, memoization

--- src/dp/WordBreak.java
+++ src/dp/WordBreak.java
@@ -4,18 +4,33 @@ import java.util.*;
 public class WordBreak {
     public boolean wordBreak(String s, List<String> wordDict) {
         HashSet<String> wordSet = new HashSet<>();
+        int n = s.length();
         for(String str:wordDict) {
             wordSet.add(str);
         }
-        // int[] dp = new int[];
-        // for(int i = 0; i< )
-        return helper(s, wordSet, s.length()-1);
+        Boolean[] dp = new Boolean[n];
+        
+        return memoization(s, wordSet, n-1, dp);
     }
 
-    public boolean helper(String s, HashSet<String> wordSet, int idx) {
+    public boolean memoization(String s, HashSet<String> wordSet, int idx, Boolean[] dp) {
+        if(idx<0) return true;
+        if(dp[idx] != null) return dp[idx];
+        Boolean res = false; 
+        for(int i =idx; i>=0; i--) {
+            if(wordSet.contains(s.substring(i, idx+1)) && memoization(s, wordSet, i - 1, dp)) {
+                res = true;
+                break;
+            }
+        }
+        dp[idx] = res;
+        return res;
+    }
+
+    public boolean recursion(String s, HashSet<String> wordSet, int idx) {
         if(idx<0) return true;
         for(int i =idx; i>=0; i--) {
-            if(wordSet.contains(s.substring(i, idx+1)) && helper(s, wordSet, i - 1)) {
+            if(wordSet.contains(s.substring(i, idx+1)) && recursion(s, wordSet, i - 1)) {
                 return true;
             }
         }
@@ -26,7 +41,7 @@ public class WordBreak {
     public static void main(String[] args) {
         WordBreak wb = new WordBreak();
         List<String> wordDict = new ArrayList<>();
-        wordDict.add("neet");
+        wordDict.add("neeta");
         wordDict.add("code");
         String s = "neetcode";
         boolean res = wb.wordBreak(s, wordDict);
